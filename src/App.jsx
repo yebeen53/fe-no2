caf4b9d4468366cbfd4e309a0d416c1a285ad6e6
yebeen53 <yebeenlee0503@naver.com>
2025-05-24 11:35:30 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 초기 진입 시 Home 보여줌 */}
         <Route path="/" element={<Home />} />
-
-        {/* 버튼 눌렀을 때 Dex 페이지로 이동 */}
         <Route path="/dex" element={<Dex />} />
-
-        {/* 포켓몬 상세 정보 */}
         <Route path="/detail/:id" element={<Detail />} />
       </Routes>
     </BrowserRouter>
