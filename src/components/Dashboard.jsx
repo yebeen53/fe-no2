699639da28c80c93aaa29a9b36a0b1a3b36e86d1
yebeen53 <yebeenlee0503@naver.com>
2025-05-25 22:26:49 +0900

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -36,28 +36,32 @@ const EmptyBox = styled.div`
   font-size: 1rem;
 `;
 
-const Dashboard = ({ selected , onRemove }) => {
+const Dashboard = ({ selected, onRemove }) => {
+  const cards = [];
+  for (let i = 0; i < 6; i++) {
+    if (selected[i]) {
+      cards.push(
+        <PoketmonCard
+          key={selected[i].id}
+          pokemon={selected[i]}
+          onClick={() => onRemove(selected[i].id)}
+          buttonLabel="삭제"
+        />
+      );
+    } else {
+      cards.push(
+        <EmptyBox key={`empty-${i}`}>포켓몬 선택</EmptyBox>
+      );
+    }
+  }
+
   return (
     <Container>
       <Title>나만의 포켓몬</Title>
-      <CardsWrapper>
-        {[...Array(6)].map((_, i) => {
-          if (selected[i]) {
-            return (
-              <PoketmonCard
-                key={selected[i].id}
-                pokemon={selected[i]}
-                onClick={() => onRemove(selected[i].id)}
-                buttonLabel="삭제"
-              />
-            );
-          } else {
-            return <EmptyBox key={`empty-${i}`}>포켓몬 선택</EmptyBox>;
-          }
-        })}
-      </CardsWrapper>
+      <CardsWrapper>{cards}</CardsWrapper>
     </Container>
   );
 };
+
 
 export default Dashboard;
